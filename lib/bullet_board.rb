7464694093ddd8db6ca053cd4e332c0fdcc233a8
gtowne <gtowne@rbmtechnologies.com>
2019-04-21 14:38:41 -0400

    First commit

---
+++ lib/bullet_board.rb
@@ -0,0 +1,6 @@
+require "bullet_board/version"
+
+module BulletBoard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/bullet_board/version.rb
@@ -0,0 +1,3 @@
+module BulletBoard
+  VERSION = "0.1.0"
+end
